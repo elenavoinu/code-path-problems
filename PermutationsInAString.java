0d3b9d5612691f4ba6359f6447f88e6898d2c8a7
Elena Voinu <37947819+elenavoinu@users.noreply.github.com>
2022-07-13 12:09:42 -0500

    Add files via upload

---
+++ PermutationsInAString.java
@@ -0,0 +1,9 @@
+public class PermutationsInAString {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasPermutationsOfPattern(String str, String pattern) {
+        
+        return false;
+    }
+}
