77763a3b329aace88c135b699d0935dda0dc5a20
Elena Voinu <37947819+elenavoinu@users.noreply.github.com>
2022-08-12 10:20:33 -0500

    Update ValidParanthesis.java

--- ValidParanthesis.java
+++ ValidParanthesis.java
@@ -11,17 +11,10 @@ public class ValidParanthesis {
         System.out.println(isValidString(string));
     }
     public static boolean isValidString(String str){
-        Map<Character, Character> mappings = new HashMap<>();
-        mappings.put('(', ')');
-        mappings.put('{', '}');
-        mappings.put('[', ']');
-
         Stack<Character> stack = new Stack();
-        for(int i = 0; i < str.length(); i++) {
-            if(mappings.containsKey(str.charAt(i))) {
-                if(stack.empty() || stack.pop() != mappings.get(str.charAt(i))) return false;
-                else
-                    stack.push(str.charAt(i));
+        for(Character ch : s.toCharArray()){
+            if(ch == ')' &&  !stack.isEmpty() && stack.peek()== '('){
+                stack.pop();
             }
         }
        return stack.empty();
@@ -45,12 +38,9 @@ U: string consists just of these characters '(', ')', '{', '}', '[', ']',
  Using stack
 
  P:
- Create a hash table and store the characters (mappings, ex: '(' is mapped to ')'
- Add the characters to the map
  Create a stack
  Iterate over the string
  Check if map contains the character in the string, push it to the stack if:
- stack.pop!=maps.get(c) or if the stack is empty
 
  return stack.empty()
 
